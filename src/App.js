73304fcb11418303a63fe568a8e05af3172de439
Wesley-Ryan <im.wesleyryan@gmail.com>
2021-05-23 11:19:13 -0700

    slanted placement update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import ParticlesBg from "particles-bg";
 import Welcome from "./pages/Welcome.js";
 import Projects from "./pages/Projects.js";
+import About from "./pages/About.js";
 import Contact from "./pages/Contact.js";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <ParticlesBg type="polygon" bg={true} num={3} />
-      <Welcome />
-      <Projects />
       <section id="slanted">
-        <Contact />
+        <ParticlesBg
+          type="polygon"
+          bg={{
+            position: "absolute",
+            zIndex: -1,
+            top: 0,
+            left: 0,
+            background: "plum",
+          }}
+          num={3}
+        />
+        <Welcome />
       </section>
+
+      <Projects />
+
+      <About />
     </div>
   );
 }
